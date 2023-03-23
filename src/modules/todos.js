4fe9c862c9e787ecbca9e774928b832783ec5e69
Sandeep K K <sandeepdotcode@gmail.com>
2023-03-23 22:50:27 +0530

    Create class to create tasks

---
+++ src/modules/todos.js
@@ -0,0 +1,10 @@
+class Todo {
+  constructor(name, description, priority, dueDate) {
+    this.name = name;
+    this.description = description;
+    this.priority = priority;
+    this.dueDate = dueDate;
+  }
+}
+
+export default Todo;
