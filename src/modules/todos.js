188d1b99ad71677f21a372c46441feb8bc9e0076
Sandeep K K <sandeepdotcode@gmail.com>
2023-03-24 08:33:31 +0530

    Add task methods

--- src/modules/todos.js
+++ src/modules/todos.js
@@ -1,10 +1,22 @@
-class Todo {
+class Task {
   constructor(name, description, priority, dueDate) {
     this.name = name;
     this.description = description;
     this.priority = priority;
     this.dueDate = dueDate;
+    this.isCompleted = false;
+  }
+
+  editTask(name, description, priority, dueDate) {
+    this.name = name;
+    this.description = description;
+    this.priority = priority;
+    this.dueDate = dueDate;
+  }
+
+  toggleCompletion() {
+    this.isCompleted = !this.isCompleted;
   }
 }
 
-export default Todo;
+export default Task;
