dfd59f7c517b51f5f277a2b62e08b55e5464bab8
Sandeep K K <sandeepdotcode@gmail.com>
2023-04-10 19:18:47 +0530

    Create fn.s to add & del. tasks & projects

--- src/modules/app-controller.js
+++ src/modules/app-controller.js
@@ -1,16 +1,39 @@
-// import createTask from './todos';
+import createTask from './todos';
 import createProject from './projects';
-import getSampleTasks from './sample-tasks';
 
 const projects = [];
 projects.push(createProject('default'));
+const currentProject = projects[0];
 
-getSampleTasks().forEach((task) => {
-  projects[0].addTask(task);
-});
-
-export function displayProjects() {
-  projects.forEach((project) => {
-    console.table(project.getTasks());
-  });
+function addNewProject(name = '') {
+  if (name === '') {
+    projects.push(createProject(`Project ${projects.length}`));
+  } else {
+    projects.push(createProject(name));
+  }
 }
+
+function getProjectIndex(name) {
+  projects.findIndex((project) => project.name === name);
+}
+
+function deleteProject(name) {
+  const index = getProjectIndex(name);
+  projects.splice(index, 1);
+}
+
+function addNewTask({
+  name, description, priority, dueDate, checkList = [],
+}) {
+  const task = createTask(name, description, priority, dueDate, checkList);
+  currentProject.addTask(task);
+}
+
+function deleteTask(name, creationTimeStamp) {
+  currentProject.removeTask(name, creationTimeStamp);
+}
+
+export {
+  addNewProject, deleteProject, addNewTask,
+  deleteTask,
+};
