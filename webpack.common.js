eaa9201e0b5c724191841719b8b468434e18d13a
Sandeep K K <sandeepdotcode@gmail.com>
2023-04-19 23:03:36 +0530

    Modify webpack config

---
+++ webpack.common.js
@@ -0,0 +1,28 @@
+// const currentTask = process.env.npm_lifecycle_event;
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const miniCSSExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+};
+
+module.exports = config;
